34ed9927252b8822d68b02ccc86126ebabf62cda
reddosakura <71519651+reddosakura@users.noreply.github.com>
2022-11-24 21:10:45 +0300

    практическая работа №7

---
+++ Arrows.cs
@@ -0,0 +1,125 @@
+﻿using System.Diagnostics;
+
+namespace ArrowName
+{
+    public class Arrows
+    {
+        public static int max;
+        public static int min;
+        public Arrows(int _min, int _max)
+        {
+            max = _max;
+            min = _min;
+        }
+    
+        public void ShowArrow(int mincoord, int step, string selector, List<string> _dirs)
+        {
+            int coord = mincoord;
+            List<string> dirs = _dirs;
+            int counter = 0;
+            while (true)
+            {
+                
+                Console.SetCursorPosition(0, coord);
+                Console.WriteLine(selector);
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (coord == 0)
+                {
+                    counter = 0;
+                }
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    if (coord - step >= min)
+                    {
+                        Console.SetCursorPosition(0, coord);
+                        Console.WriteLine("  ");
+                        coord -= step;
+                        counter += 1;
+                    }
+                }
+                else if (key.Key == ConsoleKey.DownArrow)
+                {
+                    if (coord + step < max)
+                    {
+                        Console.SetCursorPosition(0, coord);
+                        Console.WriteLine("  ");
+                        coord += step;
+                        if (counter - 1 > 0)
+                        {
+                            counter -= 1;
+                        }
+
+                    }
+                }
+                else if (key.Key == ConsoleKey.Enter)
+                {
+                    try
+                    {
+                        Console.Clear();
+                        List<string> newdircont = new List<string>();
+                        string[] allDirectories = Directory.GetDirectories(dirs[coord]);
+                        string[] allFiles = Directory.GetFiles(dirs[coord]);
+
+                        foreach (var d in allDirectories)
+                        {
+                            newdircont.Add(d);
+                        }
+
+                        foreach (var f in allFiles)
+                        {
+                            newdircont.Add(f);
+                        }
+
+                        max = newdircont.Count;
+                        // dirs = newdircont;
+                        Conductor.Сonductor.ShowDirConstent(dirs[coord]);
+
+                        dirs = newdircont;
+                    }
+                    catch (System.IO.IOException)
+                    {
+                        Process.Start(new ProcessStartInfo { FileName = dirs[coord], UseShellExecute = true});
+                    }
+                    
+                    // Console.WriteLine(string.Join(", ", dirs));
+                }
+                else if (key.Key == ConsoleKey.Backspace)
+                {
+                    string prevdir = dirs[0];
+                    List<string> dirsplit = prevdir.Split(@"\").ToList();
+                    
+                    if (dirsplit.Count > 1)
+                    {
+                        dirsplit.Remove(dirsplit.Last());
+                        dirsplit.Remove(dirsplit.Last());
+                        prevdir = string.Join(@"\", dirsplit);
+                    }
+                    
+                    Console.Clear();
+                    // Console.WriteLine($"      {prevdir}");
+                    List<string> newdircont = new List<string>();
+                    string[] allDirectories = Directory.GetDirectories(prevdir);
+                    string[] allFiles = Directory.GetFiles(prevdir);
+                    
+                    foreach (var d in allDirectories)
+                    {
+                        newdircont.Add(d);
+                    }
+                    
+                    foreach (var f in allFiles)
+                    {
+                        newdircont.Add(f);
+                    }
+                    max = newdircont.Count;
+                    dirs = newdircont;
+                    Conductor.Сonductor.ShowDirConstent(prevdir);
+                    //
+                    // dirs = newdircont;
+                    
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Сonductor.cs
@@ -0,0 +1,108 @@
+﻿using System.Globalization;
+using ArrowName;
+
+namespace Conductor
+{
+    public static class RepeatSomeValue
+{
+    public static string Repeat(this string value, int count) => string.Concat(Enumerable.Repeat(value, count));
+}
+
+public class Сonductor
+{
+    public static void ShowDirConstent(string path)
+    {
+        Console.Clear();
+        // List<List<string>> table = new List<List<string>>();
+        string[] allDirectories = Directory.GetDirectories(path);
+        string[] allFiles = Directory.GetFiles(path);
+        
+        
+        foreach (var dir in allDirectories)
+        {
+            // List<string> dirinfo = new List<string>();
+            var creation_date = Directory.GetCreationTime(dir).ToString();
+            var name = new DirectoryInfo(dir).Name;
+            try
+            {
+                // var str = $"|- Количество фалов внутри: {Directory.GetFiles(dir).Length}";
+                Console.WriteLine($"  ) Имя папки: {name} " +
+                                  $"{" ".Repeat(5)}|- Дата создания: {creation_date} " +
+                                  $"{" ".Repeat(5)}|- Количество файлов внутри: {Directory.GetFiles(dir).Length}");
+            }
+            catch (System.UnauthorizedAccessException)
+            {
+                Console.WriteLine($"  ) Имя файла: {name}" +
+                                  $"{" ".Repeat(5)}|- Дата создания: {creation_date} " +
+                                  $"{" ".Repeat(5)}|- Невозможно получить информацию о файлах внутри директории " +
+                                  $"");
+                // throw;
+            }
+        }
+
+        foreach (var f in allFiles)
+        {
+            // List<string> fileinfo = new List<string>();
+            var ext = Path.GetExtension(f);
+            var name = Path.GetFileName(f);
+            var namewithoutext = name.Split(".");
+            var creation_date = File.GetCreationTime(f).ToString();
+            Console.WriteLine($"  ) Имя файла: {namewithoutext[0]} " +
+                              $"{" ".Repeat(5)}|- Расширение файла: {ext} " +
+                              $"{" ".Repeat(5)}|- Дата создания: {creation_date} ");
+            
+        }
+    }
+    public static void init()
+    {
+
+        // Console.WriteLine(Directory.Get);
+        DriveInfo[] allDrives = DriveInfo.GetDrives();
+        List<string> drivenames = new List<string>();
+        // Console.WriteLine(allDrives.Length);
+        
+        foreach (DriveInfo d in allDrives)
+        {
+            Console.Write("  ) Диск {0}", d.Name);
+            drivenames.Add(d.Name);
+            // Console.WriteLine(" Drive type: {0}", d.DriveType);
+            if (d.IsReady)
+            {
+                string msg1 = $"  |- Файловая система: {d.DriveFormat}";
+                string msg2 = $"  |- Общий объем: {Math.Round(d.TotalSize / Math.Pow(2, 30), 2)} Gb";
+                string msg3 = $"  |- Объем свободного места: {Math.Round(d.TotalFreeSpace / Math.Pow(2, 30), 2)} Gb";
+                Console.WriteLine($"{msg1, 5}{msg2, 5}{msg3, 5}");
+            }
+        }
+        // Console.WriteLine(string.Join(", ", drivenames));
+        Arrows arr = new Arrows(0, 3);
+        arr.ShowArrow(0, 1, "->", drivenames);
+    }
+    public static void Main()
+    {
+        // ShowDirConstent(@"C:\");
+        init();
+        // string a = "a/s/f/h";
+        // List<string> b = a.Split("/").ToList();
+        // b.Remove(b.Last());
+        // a = string.Join("/", b);
+        // Console.WriteLine(a);
+        // DriveInfo[] allDrives = DriveInfo.GetDrives();
+        // string[] allDirectories = Directory.GetDirectories(allDrives[0].Name);
+
+        // // Console.WriteLine();
+        // foreach (var d in allDirectories)
+        // {
+        //     // Console.WriteLine(Path.GetDirectoryName(d));
+        // }
+        //
+        // foreach (var f in allFiles)
+        // {
+        //     // Console.Write(Path.GetExtension(Path.));
+        //     Console.WriteLine($"{f}{Path.GetFileName(f)}{Path.GetExtension(f)}");
+        // }
+    }
+}
+}
+
+
