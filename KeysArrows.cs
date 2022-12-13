94997b7bbad64e7dd819cc97a0eddc9ab02f2c8a
reddosakura <71519651+reddosakura@users.noreply.github.com>
2022-12-13 22:09:11 +0300

    Add files via upload

---
+++ KeysArrows.cs
@@ -0,0 +1,89 @@
+﻿using System.Diagnostics;
+using FullInfoN;
+using TaskManagerN;
+
+namespace ArrowName
+{
+    public class Arrows
+    {
+        internal enum keys
+        {
+            key_D = 1,
+            key_Del = 9
+        }
+        public static int max;
+        public static int min;
+        public Arrows(int _min, int _max)
+        {
+            max = _max;
+            min = _min;
+        }
+    
+        public void ShowArrow(int leftstep, int mincoord, int step, string selector, Process[] procarr)
+        {
+            // TaskManager.ShowAllProcesses();
+            int coord = mincoord;
+            // List<string> dirs = _dirs;
+            int counter = 0;
+            while (true)
+            {
+                
+                Console.SetCursorPosition(leftstep, coord);
+                Console.WriteLine(selector);
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (coord == 0)
+                {
+                    counter = 0;
+                }
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    if (coord - step >= min)
+                    {
+                        Console.SetCursorPosition(leftstep, coord);
+                        Console.WriteLine(" ");
+                        coord -= step;
+                        counter += 1;
+                    }
+                }
+                else if (key.Key == ConsoleKey.DownArrow)
+                {
+                    if (coord + step < max)
+                    {
+                        Console.SetCursorPosition(leftstep, coord);
+                        Console.WriteLine(" ");
+                        coord += step;
+                        if (counter - 1 > 0)
+                        {
+                            counter -= 1;
+                        }
+
+                    }
+                }
+                else if (key.Key == ConsoleKey.Enter)
+                {
+                    FullInfo.ShowFullInfo(procarr[counter]);
+                }
+                else if (key.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void KeyProcess(Process proc)
+        {
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            if ((int)key.Key == (int)keys.key_D)
+            {
+                TaskManager.ShowAllProcesses();
+                proc.Kill();
+
+            }
+            else if ((int)key.Key == (int)keys.key_Del)
+            {
+                TaskManager.ShowAllProcesses();
+                proc.Kill(true);
+            }
+        }
+    }
+}
---
+++ TaskManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.ComponentModel;
+using ArrowName;
+
+namespace TaskManagerN
+{
+    
+    public static class TaskManager
+    {
+        internal enum Consts
+        {
+            min = 1,
+            step = 3,
+            left = 1
+        }
+        public static void ShowAllProcesses()
+        {
+            Console.Clear();
+            Console.WriteLine("Список процессов");
+            Process[] localAll = Process.GetProcesses();
+            foreach (var proc in localAll)
+            {
+                try
+                {
+                    // List<string> procInfo = new List<string>();
+                    Console.WriteLine($"( ) - {proc.ProcessName}\n    |- Выделенная память: {proc.PagedMemorySize64 / (1024 * 1024)}MB\n    |- Время начала выполнения: {proc.StartTime}");
+                }
+                catch (System.ComponentModel.Win32Exception)
+                {
+                    // List<string> procInfo = new List<string>();
+                    Console.WriteLine($"( ) - {proc.ProcessName}\n    |- Выделенная память: {proc.PagedMemorySize64 / (1024 * 1024)}MB\n    |- Время начала выполнения: Отказано в доступе");
+                }
+            }
+
+            Arrows arrows = new Arrows((int)Consts.min, localAll.Length);
+            arrows.ShowArrow((int)Consts.left, (int)Consts.min, (int)Consts.step, "~", localAll);
+        }
+        public static void Main()
+        {
+            ShowAllProcesses();
+        }
+    }
+}
